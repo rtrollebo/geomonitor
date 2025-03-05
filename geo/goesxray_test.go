6722eb97689d51e37de1d24300d48339c420c12c
RT <rtrollebo@gmail.com>
2025-03-05 16:05:47 +0100

    Add tolerance parameter to assertion

--- geo/goesxray_test.go
+++ geo/goesxray_test.go
@@ -27,38 +27,43 @@ func TestSearchByTime(t *testing.T) {
 	array1 := []GoesXray{}
 	i, c := IndexAt(array1, time.Date(
 		2025, 01, 01, 12, 03, 00, 0, time.UTC), 1)
-	assertTimeWithintRange(t, i, c, 0)
+	assertTimeWithintRange(t, i, c, 0, 1)
 
 	array2 := []GoesXray{t1, t2, t3, t4, t5, t6, t7, t8}
 	i, c = IndexAt(array2, time.Date( //
 		2025, 01, 01, 12, 03, 00, 0, time.UTC), 1)
-	assertTimeWithintRange(t, i, c, 6)
+	assertTimeWithintRange(t, i, c, 6, 1)
 	i, c = IndexAt(array2, time.Date(
 		2025, 01, 01, 12, 01, 00, 0, time.UTC), 1)
-	assertTimeWithintRange(t, i, c, 2)
+	assertTimeWithintRange(t, i, c, 2, 1)
 	i, c = IndexAt(array2, time.Date(
 		2025, 01, 05, 12, 00, 00, 0, time.UTC), 1)
-	assertTimeWithintRange(t, i, c, 0)
+	assertTimeWithintRange(t, i, c, 0, 1)
 }
 
 func TestSearchByTimeLargeArray(t *testing.T) {
-	array := generateEquitemporalGoesXrayArray(60)
-	i, c := IndexAt(array, time.Date(
+	array1 := generateEquitemporalGoesXrayArray(60)
+	i, c := IndexAt(array1, time.Date(
 		2025, 01, 01, 12, 15, 00, 0, time.UTC), 2)
-	assertTimeWithintRange(t, i, c, 30)
-	i, c = IndexAt(array, time.Date(
+	assertTimeWithintRange(t, i, c, 30, 1)
+	i, c = IndexAt(array1, time.Date(
 		2025, 01, 01, 12, 45, 00, 0, time.UTC), 2)
-	assertTimeWithintRange(t, i, c, 90)
-	i, c = IndexAt(array, time.Date(
+	assertTimeWithintRange(t, i, c, 90, 1)
+	i, c = IndexAt(array1, time.Date(
 		2025, 01, 01, 12, 25, 00, 0, time.UTC), 2)
-	assertTimeWithintRange(t, i, c, 50)
+	assertTimeWithintRange(t, i, c, 50, 1)
+
+	array2 := generateEquitemporalGoesXrayArray(60*60*24)
+	i, c = IndexAt(array2, time.Date(2025, 01, 02, 8, 00, 00, 0, time.UTC), 2)
+	assertTimeWithintRange(t, i, c, (12+8)*60*2, 3)
+
 }
 
-func assertTimeWithintRange(t *testing.T, idx, c, centerValue int) {
+func assertTimeWithintRange(t *testing.T, idx, c, centerValue int, tolerance int) {
 	if c > (centerValue / 2) {
 		t.Errorf("Too many iterations")
 	}
-	if idx < (centerValue-1) || idx > (centerValue+1) {
+	if idx < (centerValue-tolerance) || idx > (centerValue+tolerance) {
 		t.Errorf("Failed: %d %d", idx, c)
 	}
 }
